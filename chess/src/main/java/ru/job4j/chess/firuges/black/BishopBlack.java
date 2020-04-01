bcfbceff4d7f835401c83a5fba52e26ca1614712
ivanrightman <vivasvse@gmail.com>
2020-04-01 08:57:14 +0500

    шахматы

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -22,27 +22,28 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", source, dest)
-        );
-//        if (!isDiagonal(source, dest)) {
-//            throw new IllegalStateException(
-//                    String.format("Could not way by diagonal from %s to %s", source, dest)
-//            );
-//        }
-//        int size = ...;
-//        Cell[] steps = new Cell[size];
-//        int deltaX = ...;
-//        int deltaY = ...;
-//        for (int index = 0; index < size; index++) {
-//            steps[index] = ...
-//        }
-//        return steps;
+        if (!isDiagonal(source, dest)) {
+            throw new IllegalStateException(
+                  String.format("Could not way by diagonal from %s to %s", source, dest)
+         );
+      }
+        int size = dest.x - source.x;
+        Cell[] steps = new Cell[size];
+        int deltaX = dest.x > source.x ? source.x + 1 : dest.x -1;
+        int deltaY = dest.y > source.y ? source.y + 1 : dest.y -1;
+        for (int index = 0; index < size; index++) {
+            int x = index + deltaX;
+            int y = index + deltaY;
+            steps[index] = Cell.findBy(x, y);
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        //TODO check diagonal
-        return false;
+        return dest.x + source.x == dest.y - source.y
+                || dest.x + source.x == source.y - dest.y
+                || dest.x - source.x == source.y - dest.y
+                || dest.x - source.x == dest.y - source.y;
     }
 
     @Override
---
+++ chess/src/test/java/ru/job4j/BishopBlackTest.java
@@ -0,0 +1,41 @@
+package ru.job4j;
+
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class BishopBlackTest {
+    @Test
+    public void positionSame() {
+        Figure bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.position();
+        assertThat(bishopBlack.position(), is(Cell.C1));
+    }
+
+    @Test
+    public void copySame() {
+        Figure bishopBlack = new BishopBlack(Cell.C1);
+        Figure bishopBlackCopy = bishopBlack.copy(Cell.A3);
+        assertThat(bishopBlackCopy.position(), is(Cell.A3));
+    }
+
+    @Test
+    public void wayDiagonal() {
+        Figure bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Cell[] result = bishopBlack.way(Cell.C1, Cell.G5);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void wayNotDiagonal() {
+        Figure bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Cell[] result = bishopBlack.way(Cell.C1, Cell.H5);
+        assertThat(result, is(expected));
+    }
+}
